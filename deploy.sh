f3e953d7baa9f80478fdb40d1bbd74d2114a77f6
yoonseo <yoonseo8000000@gmail.com>
2026-07-02 23:34:25 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 cd /Users/yoonseo/bookbot
 python3 crawl.py
 cd /Users/yoonseo/Desktop/blog
-hugo --cleanDestinationDir
-git submodule update --init --recursive
+find public -mindepth 1 -maxdepth 1 ! -name '.git' -exec rm -rf {} +
+hugo
 git add .
 git commit -m "auto update"
 git push
